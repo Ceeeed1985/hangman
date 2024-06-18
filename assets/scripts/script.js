6c7d4618981dbbeb5c1c26dcd2d4a26c6850093c
Cedric Truyen <cedrictruyen@icloud.com>
2024-06-18 11:00:37 +0200

    Ajout des id dans mes boutons du menu

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -11,11 +11,11 @@ boutonStart.addEventListener("click", () => {
     let choixJeu = document.getElementById("ecranDeJeu");
     choixJeu.innerHTML = `
     <h2>Faites votre choix</h2>
-    <button class="btnListeFilms">${boutonFilms}</button>
-    <button class="btnListePersonnages">${boutonPersonnages}</button>
-    <button class="btnListeAnimaux">${boutonAnimaux}</button>
-    <button class="btnListeMots">${boutonMots}</button>
-    <button class="btnListeVerbes">${boutonVerbes}</button>
+    <button id="btrListeFilms" class="btnListeFilms">${boutonFilms}</button>
+    <button id="btnListePersonnages" class="btnListePersonnages">${boutonPersonnages}</button>
+    <button id="btnListeAnimaux" class="btnListeAnimaux">${boutonAnimaux}</button>
+    <button id="btnListeMots" class="btnListeMots">${boutonMots}</button>
+    <button id="btnListeMots" class="btnListeVerbes">${boutonVerbes}</button>
     `
 });
 
