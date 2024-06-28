59e31cedf04ed924c28ac2f1ce45758296eeed17
Cedric Truyen <cedrictruyen@icloud.com>
2024-06-28 12:43:30 +0200

    Amélioration des images + création d'une fonction addAnimation

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,11 +4,11 @@ let dashWord = "";
 const hiddenWordElement = document.getElementById("hiddenWord");
 const buttons = document.querySelectorAll(".btnKeyboard");
 const unusedButtons = document.querySelectorAll(".btnDeco");
-//JE DOIS ECRIRE CES DEUX LIGNES CONVENABLEMENT - A VERIFIER !!!!!!
 let countElement = document.getElementById("count");
 let count = parseInt(countElement.textContent);
 let tableauScoreElement = document.getElementById("tableauScore");
-console.log(count);
+let cartePenduElement = document.querySelector("#cartePendu img");
+console.log(cartePenduElement);
 let choosenLetter;
 
 
@@ -17,11 +17,12 @@ let randomIndex = Math.floor(Math.random() * array.length);
 wordToGuess = (array[randomIndex]);
 wordLength = wordToGuess.length;
 dashWord = "_".repeat(wordLength);
-    // for (i = 0; i < wordLength; i++){
-    //     dashWord += "_";
-    // }
-// console.log(wordToGuess);
-// console.log(wordLength);
+}
+
+function addAnimation(element, animationClass){
+    element.classList.remove("animate__animated", animationClass);
+    void element.offsetWidth;
+    element.classList.add("animate__animated", animationClass);
 }
 
 chooseWord(movieList);
@@ -41,6 +42,7 @@ buttons.forEach(button =>{
         choosenLetter = button.textContent;
         let newDashWord = "";
         let foundLetter = false;
+
         for (let i = 0; i < wordToGuess.length; i++){
             if (wordToGuess[i].toLowerCase() === choosenLetter.toLowerCase()){
                 newDashWord += choosenLetter;
@@ -48,25 +50,29 @@ buttons.forEach(button =>{
                 foundLetter = true;
             } else {
                 newDashWord += dashWord[i];
-            }
-            
+            }  
         }
+
         if(!foundLetter){
             button.classList.add("missed");
             count-= 1;
             countElement.textContent = count;
             
-            tableauScoreElement.classList.remove("animate__animated", "animate__bounceInRight");
-            void tableauScoreElement.offsetWidth;
-            tableauScoreElement.classList.add("animate__animated", "animate__bounceInRight");
-
+            addAnimation(tableauScoreElement, "animate__bounceInRight");
+            addAnimation(cartePenduElement, "animate__bounceInLeft");
 
             tableauScoreElement.classList.remove("decompte4", "decompte3", "decompte2", "decompte1", "gameOver", "defaultClass");
 
             switch(count){
+                case 5:
+                    cartePenduElement.src = "assets/pictures/carte_count_5.png";
+                    break;
                 case 4:
-                case 3:
+                    cartePenduElement.src = "assets/pictures/carte_count_4.png";
                     tableauScoreElement.classList.add("decompte4");
+                    break;
+                case 3:
+                    tableauScoreElement.classList.add("decompte3");
                     break;
                 case 2:
                     tableauScoreElement.classList.add("decompte2");
@@ -82,19 +88,19 @@ buttons.forEach(button =>{
                 default:
                     tableauScoreElement.classList.add("defaultClass");
             }
+           
         }
 
-        
-        // if (count == 0){
-        //     hiddenWordElement.innerHTML = "GAME OVER";
-        //     buttons.forEach(btn => btn.disabled = true);
-        //     return;
-        // }
-
-
+        if (newDashWord === wordToGuess) {
+            hiddenWordElement.innerHTML = "Bravo!";
+            buttons.forEach(btn => btn.disabled = true);
+            return; // Sortie de la fonction si le jeu est gagné
+        }
 
         dashWord = newDashWord;
         hiddenWordElement.innerHTML = dashWord;
+
+        
     })
 
 })
