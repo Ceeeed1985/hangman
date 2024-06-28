7b3cb6b535daeeaaec9a55c77cff8de6782e81b1
Cedric Truyen <cedrictruyen@icloud.com>
2024-06-28 10:54:37 +0200

    Amélioration du CSS - ajout d'animations et decompte fonctionnel

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,8 +5,10 @@ const hiddenWordElement = document.getElementById("hiddenWord");
 const buttons = document.querySelectorAll(".btnKeyboard");
 const unusedButtons = document.querySelectorAll(".btnDeco");
 //JE DOIS ECRIRE CES DEUX LIGNES CONVENABLEMENT - A VERIFIER !!!!!!
-// let count = document.getElementById("count");
-// console.log(count);
+let countElement = document.getElementById("count");
+let count = parseInt(countElement.textContent);
+let tableauScoreElement = document.getElementById("tableauScore");
+console.log(count);
 let choosenLetter;
 
 
@@ -38,19 +40,63 @@ buttons.forEach(button =>{
     button.addEventListener("click", () => {
         choosenLetter = button.textContent;
         let newDashWord = "";
+        let foundLetter = false;
         for (let i = 0; i < wordToGuess.length; i++){
             if (wordToGuess[i].toLowerCase() === choosenLetter.toLowerCase()){
                 newDashWord += choosenLetter;
                 button.classList.add("success");
+                foundLetter = true;
             } else {
                 newDashWord += dashWord[i];
-                button.classList.add("missed");
+            }
+            
+        }
+        if(!foundLetter){
+            button.classList.add("missed");
+            count-= 1;
+            countElement.textContent = count;
+            
+            tableauScoreElement.classList.remove("animate__animated", "animate__bounceInRight");
+            void tableauScoreElement.offsetWidth;
+            tableauScoreElement.classList.add("animate__animated", "animate__bounceInRight");
 
+
+            tableauScoreElement.classList.remove("decompte4", "decompte3", "decompte2", "decompte1", "gameOver", "defaultClass");
+
+            switch(count){
+                case 4:
+                case 3:
+                    tableauScoreElement.classList.add("decompte4");
+                    break;
+                case 2:
+                    tableauScoreElement.classList.add("decompte2");
+                    break;
+                case 1:
+                    tableauScoreElement.classList.add("decompte1");
+                    break;
+                case 0:
+                    tableauScoreElement.classList.add("gameOver");
+                    hiddenWordElement.innerHTML = "GAME OVER";
+                    buttons.forEach(btn => btn.disabled = true);
+                    return;
+                default:
+                    tableauScoreElement.classList.add("defaultClass");
             }
         }
+
+        
+        // if (count == 0){
+        //     hiddenWordElement.innerHTML = "GAME OVER";
+        //     buttons.forEach(btn => btn.disabled = true);
+        //     return;
+        // }
+
+
+
         dashWord = newDashWord;
         hiddenWordElement.innerHTML = dashWord;
     })
+
 })
 
 
